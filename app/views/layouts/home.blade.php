da7485dd8e588afa6c12dbc27f8fb00fec76eef1
Carlos Narez <cnarez@gmail.com>
2014-06-26 11:04:20 -0600

    Adding home styles

---
+++ app/views/layouts/home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Mundialito 2014</title>
+    <link href='http://fonts.googleapis.com/css?family=Asap:400,700,700italic,400italic' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="ui/css/styles.css" />
+</head>
+<body>
+    <div class="container">
+    @yield('content')
+    </div>
+</body>
+</html>
